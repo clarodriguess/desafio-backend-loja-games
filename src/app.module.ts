2c083b8b46327359b3f467b35b3e4accc8d4ae56
clarisse <clarissebleasby@gmail.com>
2026-03-09 21:37:13 -0300

    👌 IMPROVE:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Produto } from './produto/entitites/produto.entity';
 import { Categoria } from './categoria/entitites/categoria.entity';
